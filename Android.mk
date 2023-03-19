1b0bc84a3f81046dcbc5e1bd83760989805fc73d
kaptain756464 <126095024+kaptain756464@users.noreply.github.com>
2023-03-19 06:22:10 +0200

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,34 +1,13 @@
-# Copyright (C) 2022 The Android Open Source Project
-# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
 # SPDX-License-Identifier: Apache-2.0
+#
 
-LOCAL_PATH := device/xiaomi/apollo
+LOCAL_PATH := $(call my-dir)
 
-# Dynamic Partitions
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
+ifeq ($(TARGET_DEVICE),apollo)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
 
-# fastboot
-PRODUCT_PACKAGES := android.hardware.fastboot@1.0-impl-mock
-
-# fastbootd
-PRODUCT_PACKAGES := fastbootd
-
-# QCom Decryption
-PRODUCT_PACKAGES += \
-    qcom_decrypt \
-    qcom_decrypt_fbe
-
-# Soong Namespaces
-PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
-
-
-
-# API Level
-PRODUCT_SHIPPING_API_LEVEL := 29
-BOARD_SHIPPING_API_LEVEL := 29
-BOARD_API_LEVEL := 29
-SHIPPING_API_LEVEL := 29
-
-# Assert
-TARGET_OTA_ASSERT_DEVICE := M2007J3SY
